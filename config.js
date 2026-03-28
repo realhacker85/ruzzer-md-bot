cb558b26d00048b3431c0905ae70fd8a73f7741b
realhacker85 <realhacker85@gmail.com>
2026-03-29 02:57:21 +0300

    Create config.js with bot configuration

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    botName: "꧁༒ 𝖗𝖚𝖘𝖘𝖊𝖗 MD༒꧂",
+    ownerName: "𝖗𝖚𝖘𝖘𝖊𝖗 𝖙𝖊𝖈𝖍",
+    ownerNumber: "+255651373899",
+    telegramToken: process.env.TELEGRAM_TOKEN,
+    whatsappChannelLink: "https://example.com/whatsapp", // Replace with the actual link
+    prefix: "!", // Define your command prefix
+    customConfig: {
+        // Add any other custom configurations here
+    }
+};
